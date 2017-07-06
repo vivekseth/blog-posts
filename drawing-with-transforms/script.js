93d4cfcf08884fea9ce800d9bc9307cbfa5dbe55
Vivek Seth <v_seth@apple.com>
2017-07-06 14:00:52 -0700

    Parametric function + animation

--- drawing-with-transforms/script.js
+++ drawing-with-transforms/script.js
@@ -26,33 +26,6 @@ function drawLine(ctx, x1, y1, x2, y2) {
     ctx.stroke();   
 }
 
-
-// function drawParametric(ctx, fx, fy, tarr) {
-//     for (var i=0; i<(tarr.length - 1); i++) {
-//         var x1 = fx(tarr[i]);
-//         var y1 = fy(tarr[i]);
-//         var x2 = fx(tarr[i + 1]);
-//         var y2 = fy(tarr[i + 1]);
-//         drawLine(ctx, x1, y1, x2, y2);
-//     }
-// }
-
-
-// Code Sample
-// drawParametric(ctx, function(t) {
-//     return t
-// }, function(t) {
-//     return t
-// }, [200, 400]);
-
-
-// drawParametric(ctx, function(t) {
-//     return 600 - t
-// }, function(t) {
-//     return t
-// }, [200, 400]
-
-
 function range(low, high, N) {
     var delta = (high - low);
     var step = delta / N;
@@ -65,46 +38,29 @@ function range(low, high, N) {
     return arr;
 }
 
-
-// Code sample
-// drawParametric(ctx, function(t) {
-//     return 100 * Math.cos(t)
-// }, function(t) {
-//     return 100 * Math.sin(t)
-// }, range(0, 2 * Math.PI, 100));
-
-
-// Code sample
-// drawParametric(ctx, function(t) {
-//     return 100 * Math.cos(t) + 300
-// }, function(t) {
-//     return 100 * Math.sin(t) + 300
-// }, range(0, 2 * Math.PI, 100));
-
-
 // New drawParametric
 function drawParametric(ctx, fx, fy, tarr) {
     function center(coordinate, dimensionLength) {
         return (coordinate + 0.5) * dimensionLength;
     }
 
-    for (var i=0; i<(tarr.length - 1); i++) {
-        var x1 = center(fx(tarr[i]), WIDTH);
-        var y1 = center(fy(tarr[i]), HEIGHT);
-        var x2 = center(fx(tarr[i + 1]), WIDTH);
-        var y2 = center(fy(tarr[i + 1]), HEIGHT);
-        drawLine(ctx, x1, y1, x2, y2);
+    var first = true;
+
+    ctx.beginPath();
+    for (var i=0; i<tarr.length; i++) {
+        var x = center(fx(tarr[i]), WIDTH);
+        var y = center(fy(tarr[i]), HEIGHT);
+
+        if (first) {
+            ctx.moveTo(x, y);
+            first = false;
+        }
+        else {
+            ctx.lineTo(x, y);
+        }
     }
+    ctx.stroke();
 }
-
-
-// Code Sample
-// drawParametric(ctx, function(t) {
-//     return 0.1 * Math.cos(t)
-// }, function(t) {
-//     return 0.1 * Math.sin(t)
-// }, range(0, 2 * Math.PI, 100));
-
 
 function drawPolar(ctx, fth, tr, tarr) {
     var fx = function(t) {
@@ -118,56 +74,21 @@ function drawPolar(ctx, fth, tr, tarr) {
     drawParametric(ctx, fx, fy, tarr);
 }
 
+var animation = function(timestamp){
+    var x = (10 * (0.5 * (Math.cos(0.0005 * timestamp) + 1.0)) + 150);
 
-// Code Sample 
-// drawPolar(ctx, function(t) {
-//     return t
-// }, function(t) {
-//     return 0.1
-// }, range(0, 2 * Math.PI, 100))
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
 
+    // Code Sample
+    ctx.lineWidth=0.3; // (Math.cos(0.01 * timestamp) * 0.3) + 0.4;
+    drawPolar(ctx, function(t){
+        return t
+    }, function(t){
+        return (0.5) * Math.sin(t / 0.7);
+    }, range(0, 200, x));
 
-// Code Sample
-// ctx.lineWidth=0.2;
-// drawPolar(ctx, function(t){
-//     return t
-// }, function(t){
-//     return 0.4 * Math.sin(t/1.2)
-// }, range(0, 38, 800));
+    requestAnimationFrame(animation);
+}
 
-
-// Code Sample
-// ctx.lineWidth=0.2;
-// drawPolar(ctx, function(t){
-//     return t
-// }, function(t){
-//     return 0.4 * Math.sin(t/1.2)
-// }, range(0, 500, 800));
-
-
-// Code Sample
-// ctx.lineWidth=0.2;
-// drawPolar(ctx, function(t){
-//     return t
-// }, function(t){
-//     return 0.005 * t
-// }, range(0, 100, 800));
-
-
-// Code Sample
-// ctx.lineWidth=0.2;
-// drawPolar(ctx, function(t){
-//     return t
-// }, function(t){
-//     return 0.005 * t
-// }, range(0, 100, 80));
-
-
-// Code Sample
-ctx.lineWidth=10;
-drawPolar(ctx, function(t){
-    return t
-}, function(t){
-    return 0.1 * Math.sin(t / 0.5) * Math.sin(t / 0.2) + t * 0.018
-}, range(-200, 200, 8000));
+requestAnimationFrame(animation);
 
